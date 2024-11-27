c3116713e23d4c4938f591bc9e75934f865f5e4c
miguelmanabo4 <miguelmanabo4@gmail.com>
2024-11-27 17:11:30 +0000

    Created Colord titles and removed borders

--- src/components/TaskBox.jsx
+++ src/components/TaskBox.jsx
@@ -1,24 +1,24 @@
 import { RiDeleteBackLine } from "react-icons/ri";
-
-//bg-gradient-to-br from-[#BCECFF] to-[#C2BBFF]
+//bg-gradient-to-br from-[#FFD1FF] to-[#FBD0C8] - Red / Pending
+//bg-gradient-to-br from-[#97F7F5] to-[#FBF7B8] - Green / Completed
 function TaskBox() {
     return (
         <>
             <div class="h-[230px] flex justify-center items-center">
 
-                <div class="border-[1px] border-black w-[280px] rounded-[10px] group"> {/* For Border */}
+                <div class=" w-[280px] rounded-[10px] group bg-[#ffffff80]"> {/* For Border */}
 
                     <div class="grid grid-cols-[repeat(2,50%)] h-[200px] grid-rows-[40px_100px_30px]"> {/* For Grid */}
 
                         <div class="flex justify-start ml-[10px] items-center">
-                            <div class="border-[1px] border-black px-[30px] rounded-[10px]"> Title </div>
+                            <div class="bg-gradient-to-br from-[#97F7F5] to-[#FBF7B8] border-black px-[30px] rounded-[10px]"> Title </div>
                         </div>
 
                         <div class="flex justify-end items-center mr-[10px] hidden group-hover:flex transition-opacity duration-300">
                             <RiDeleteBackLine class="w-6 h-6"/>
                         </div>
 
-                        <div class=" col-span-2 row-span-2 w-[260px] ml-[9px] w-[260px]"> 
+                        <div class=" col-span-2 row-span-2 w-[260px] ml-[9px] w-[260px] font-[Finlandica]"> 
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed dolore, magnam iure quas libero dolorum voluptate maiores modi maxime corrupti. 
                         </div>
 
