63b4c64cf28ab001fdc37617ebe29fc64b99355b
miguelmanabo4 <miguelmanabo4@gmail.com>
2024-11-27 12:43:21 +0000

    Fixed content in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{html,js}', 
-    './public/index.html',  
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './public/index.html',
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
   ],
   theme: {
     extend: {},
