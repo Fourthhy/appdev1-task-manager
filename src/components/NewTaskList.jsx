6fc7d8af0593333be0e72fdaf271c5015e37af20
miguelmanabo4 <miguelmanabo4@gmail.com>
2024-12-18 15:00:24 +0000

    Made changes for update

---
+++ src/components/NewTaskList.jsx
@@ -0,0 +1,9 @@
+function NewTaskList() {
+    return (
+        <>
+            <h3>Hello, Universe</h3>
+        </>
+    )
+}
+
+export default NewTaskList;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import DarkMode from './components/DarkMode.jsx'
-import Drop from './components/Drop.jsx'
+import NewTaskList from './components/NewTaskList.jsx'
+
 
 createRoot(document.getElementById('root')).render(
-    <App />,
+    <StrictMode>
+        <NewTaskList />
+    </StrictMode>
 )
