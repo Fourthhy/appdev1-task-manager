26803c692cd28404758617830d118466e451b1c0
miguelmanabo4 <miguelmanabo4@gmail.com>
2024-12-19 11:53:46 +0000

    Created Sign Out Component

--- src/components/NewTaskList.jsx
+++ src/components/NewTaskList.jsx
@@ -1,8 +1,8 @@
 import NewAddTask from "./NewAddTask";
 import SignIn from "./SignIn";
+import SignOut from "./SignOut";
 import SignUp from "./SignUp";
 import TaskItem from "./TaskItem";
-
 
 function NewTaskList() {
     return (
@@ -10,7 +10,23 @@ function NewTaskList() {
             <div className="h-screen w-screen bg-[#000010] flex justify-center items-center"> {/* MAIN CONTAINER */}
                 <div className="bg-[#ffffff20] rounded-[10px] border-[1px] border-[#ffffff75] ">
                     <div className="h-[97vh] w-[98vw] overflow-y-auto overflow-x-hidden ">
-                        <SignUp />
+                        <NewAddTask />
+                        <div className="flex justify-between items-center">
+                            <h3 className="text-[#ffffffdd] font-Raleway text-[20px] px-[10px] py-[10px]">
+                                Welcome, User
+                            </h3>
+                            <div className="mr-[10px]">
+                                <SignOut />
+                            </div>
+                        </div>
+                        
+                        <div className="grid grid-cols-4">
+                            <TaskItem title='sample title' desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, qui.' status='pending'/>
+                            <TaskItem title='sample title' desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, qui.' status='pending'/>
+                            <TaskItem title='sample title' desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, qui.' status='pending'/>
+                            <TaskItem title='sample title' desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, qui.' status='pending'/>                                
+                            <TaskItem title='sample title' desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, qui.' status='pending'/>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/components/SignOut.jsx
@@ -0,0 +1,9 @@
+function SignOut() {
+    return (
+        <>
+            <button type="submit" className="border-[1.5px] border-[#ffffff99] py-[5px] px-[10px] rounded-[10px] font-Raleway text-[#ffffff99] bg-[#ffffff20] w-[100px]">Sign Out</button>
+        </>
+    )
+}
+
+export default SignOut 
